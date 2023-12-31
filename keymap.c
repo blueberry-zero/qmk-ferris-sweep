8c3b25bbbec88e274f2c036d4991a182dce20c66
Saad Khan <khan.ssjc@gmail.com>
2023-12-30 23:43:17 -0500

    feat: wip board

--- keymap.c
+++ keymap.c
@@ -1,70 +1,92 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+	//Base Layer (alpha, punct)
 	[0] = LAYOUT_split_3x5_2(
 		// row 1 right - left
-		KC_P,		KC_O,		KC_I,		KC_U,		KC_Y,
-		KC_Q,		KC_W,		KC_E,		KC_R,		KC_T,
+		KC_QUOT,	KC_Y,		KC_U,		KC_L,		KC_J,
+		KC_B,		KC_P,		KC_F,		KC_W,		KC_Q,
 
 		// row 2 right - left
-		LSFT_T(KC_A),	LT(5,KC_S),	LT(1,KC_D),	LT(3,KC_F),	KC_G,
-		KC_H,		LT(4,KC_J),	LT(2,KC_K),	LT(6,KC_L),	LSFT_T(KC_SCLN),
+		RGUI_T(KC_O),	RALT_T(KC_I),	RSFT_T(KC_E),	RCTL_T(KC_N),	KC_M,
+		KC_G,		LCTL_T(KC_T),	LSFT_T(KC_S),	LALT_T(KC_R),	LGUI_T(KC_A),
 		
 		// row 3 right - left
-		KC_Z,		LCTL_T(KC_X),	LALT_T(KC_C),	KC_V,		KC_B,
-		KC_N,		KC_M,		LALT_T(KC_COMM),LCTL_T(KC_DOT),	KC_SLSH,
+		KC_SLSH,	KC_DOT,		KC_COMM,	KC_H,		KC_K,
+		KC_V,		KC_D,		KC_C,		KC_X,		KC_Z,
 
 		// row 4
-		KC_P0,		KC_BSPC,	LT(7,KC_SPC),	KC_P1
+		LT(KC_BSPC,1),	LT(KC_ENT,2),	LT(KC_TAB,3),	LT(KC_SPC,4)
 	),
+	// Function Layer
 	[1] = LAYOUT_split_3x5_2(
 		// row 1 right - left
 		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,
-		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,
+		KC_F1,		KC_F2,		KC_F3,		KC_F4,		KC_PCSR,
 
 		// row 2 right - left
-		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,
-		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,
+		KC_RGUI,	KC_RALT,	KC_RSFT,	KC_RCTL,	KC_NO,
+		KC_F5,		KC_F6,		KC_F7,		KC_F8,		KC_ESC,
 
 		// row 3 right - left
 		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,
-		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,
+		KC_F9,		KC_F10,		KC_F11,		KC_F12,		PC_SCRL,
 		
 		// row 4
-		KC_NO,		KC_NO,		KC_NO,		KC_NO
+		KC_TRANS,	KC_TRANS,	KC_TRANS,	KC_TRANS
 	),
+	// symbol layer
 	[2] = LAYOUT_split_3x5_2(
 		// row 1 right - left
 		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,
-		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,
+		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_ESC,
 
 		// row 2 right - left
-		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,
-		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,
+		KC_LGUI,	KC_LALT,	KC_LSFT,	KC_LCTL,	KC_ESC,
+		KC_ESC,		KC_RCTL,	KC_RSFT,	KC_RALT,	LC_RGUI,
 
 		// row 3 right - left
 		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,
 		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,
 		
 		// row 4
-		KC_NO,		KC_NO,		KC_NO,		KC_NO
+		KC_TRANS,	KC_TRANS,	KC_TRANS,	KC_TRANS
 	),
+	// number layer
 	[3] = LAYOUT_split_3x5_2(
 		// row 1 right - left
-		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,
+		KC_ESC,		KC_NO,		KC_NO,		KC_NO,		KC_NO,
 		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,
 
 		// row 2 right - left
-		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,
-		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,
+		KC_LGUI,	KC_LALT,	KC_LSFT,	KC_LCTL,	KC_ESC,
+		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,		
 
 		// row 3 right - left
 		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,
 		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,
 		
 		// row 4
-		KC_NO,		KC_NO,		KC_NO,		KC_NO
+		KC_TRANS,	KC_TRANS,	KC_TRANS,	KC_TRANS
 	),
+	// nav layer
+	[4] = LAYOUT_split_3x5_2(
+		// row 1 right - left
+		KC_INS,		KC_DEL,		KC_HOME,	KC_END,		KC_NO,
+		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,
+
+		// row 2 right - left
+		KC_RIGHT,	KC_LEFT,	KC_UP,		KC_DOWN,	KC_ESC,
+		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,
+
+		// row 3 right - left
+		KC_NO,		KC_NO,		KC_PGUP,	KC_PGDOWN,	KC_NO,
+		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,
+		
+		// row 4
+		KC_TRANS,	KC_TRANS,	KC_TRANS,	KC_TRANS
+	),
+
 };
 
 #if defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
