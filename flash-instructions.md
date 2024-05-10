5237d88804200f7a9a43ea00d38eba9ea9ca178a
Saad Khan <khan.ssjc@gmail.com>
2024-05-10 00:52:03 -0400

    feat: updated key map

---
+++ flash-instructions.md
@@ -0,0 +1,15 @@
+# INSTRUCTIONS
+
+1. Flash command
+
+    `qmk flash -kb <keyboard_name> -km <keymap_name> -e CONVERT_TO=promicro_rp2040`
+
+    `qmk flash -kb ferris/sweep -km khan-ssjc -e CONVERT_TO=promicro_rp2040`
+
+2. Reset pins on keyboard 
+3. Open keyboard folder in file system (or more complicated mount)
+    
+
+## Resources
+
+1. [JoshJohnson](https://joshajohnson.com/sea-picro/)
--- keymap.c
+++ keymap.c
@@ -5,45 +5,48 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT_split_3x5_2(
 		// row 1 right - left
 		// pinky	ring		middle		index		index+
-		KC_QUOT,	KC_Y,		KC_U,		KC_L,		KC_J,
+		KC_O,     KC_I,     KC_U,     KC_Y,     KC_NO,
 		// index+	index		middle		ring		pinky
-		KC_B,		KC_P,		KC_F,		KC_W,		KC_Q,
+		KC_NO,    KC_R,		  KC_E,		  KC_W,		  KC_Q,
 
 		// row 2 right - left
 		// pinky	ring		middle		index		index+
-		RGUI_T(KC_O),	RALT_T(KC_I),	RSFT_T(KC_E),	RCTL_T(KC_N),	KC_M,
+		RGUI_T(KC_L),	RALT_T(KC_K),	RSFT_T(KC_J),	RCTL_T(KC_H),	KC_NO,
 		// index+	index		middle		ring		pinky
-		KC_G,		LCTL_T(KC_T),	LSFT_T(KC_S),	LALT_T(KC_R),	LGUI_T(KC_A),
+		KC_NO,  LCTL_T(KC_F),	LSFT_T(KC_D),	LALT_T(KC_S),	LGUI_T(KC_A),
+
+    // DONE
 		
 		// row 3 right - left
 		// pinky	ring		middle		index		index+
-		KC_SLSH,	KC_DOT,		KC_COMM,	KC_H,		KC_K,
+		KC_DOT,   KC_COMM,  KC_M,     KC_N,     KC_NO,
 		// index+	index		middle		ring		pinky
-		KC_V,		KC_D,		KC_C,		KC_X,		KC_Z,
+		KC_NO,    KC_V,     KC_C,     KC_X,		  KC_Z,
 
 		// row 4
-		// lthumb_i	lthumb_o	rthumb_o	rthumb_i
-		LT(1, KC_BSPC),	LT(2, KC_ENT),	LT(3, KC_SPC),	LT(4, KC_TAB)
+		// rthumb_o     rthumb_i	      lthumb_i	      lthumb_o
+		LT(1, KC_DEL),	LT(2, KC_BSPC),	LT(3, KC_TAB),	LT(4, KC_SPC)
 	),
+  // 
 	// Function Layer
 	[1] = LAYOUT_split_3x5_2(
 		// row 1 right - left
 		// pinky	ring		middle		index		index+
 		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,
 		// index+	index		middle		ring		pinky
-		KC_PSCR,	KC_F4,		KC_F3,		KC_F2,		KC_F1,
+		KC_NO,	  KC_0,		  KC_9,		  KC_ESC,		KC_GRV,
 
 		// row 2 right - left
 		// pinky	ring		middle		index		index+
-		KC_RGUI,	KC_RALT,	KC_RSFT,	KC_RCTL,	KC_ESC,
+		KC_RGUI,	KC_RALT,	KC_RSFT,	KC_RCTL,	KC_NO,
 		// index+	index		middle		ring		pinky
-		KC_ESC,		KC_F8,		KC_F7,		KC_F6,		KC_F5,
+		KC_NO,		KC_4,		  KC_3,		  KC_2,		  KC_1,
 
 		// row 3 right - left
 		// pinky	ring		middle		index		index+
 		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,
 		// index+	index		middle		ring		pinky
-		KC_SCRL,	KC_F12,		KC_F11,		KC_F10,		KC_F9,
+		KC_NO,  	KC_8,		  KC_7,		  KC_6,		  KC_5,
 		
 		// row 4
 		// lthumb_i	lthumb_o	rthumb_o	rthumb_i
@@ -55,19 +58,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		// pinky	ring		middle		index		index+
 		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,
 		// index+	index		middle		ring		pinky
-		KC_NO,		KC_RCBR,	KC_LCBR,	KC_NO,		KC_NO,
+		KC_NO,		KC_F8,    KC_F7,    KC_F6,    KC_F5,
 
 		// row 2 right - left
 		// pinky	ring		middle		index		index+
-		KC_RGUI,	KC_RALT,	KC_RSFT,	KC_RCTL,	KC_ESC,
+		KC_RGUI,	KC_RALT,	KC_RSFT,	KC_RCTL,	KC_NO,
 		// index+	index		middle		ring		pinky
-		KC_ESC,		KC_RBRC,	KC_LBRC,	KC_SCLN,	KC_BSLS,
+		KC_NO,		KC_F4,    KC_F3,    KC_F2,    KC_F1,
 
 		// row 3 right - left
 		// pinky	ring		middle		index		index+
 		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,
 		// index+	index		middle		ring		pinky
-		KC_NO,		KC_RPRN,	KC_LPRN,	KC_NO,		KC_NO,
+		KC_NO,		KC_F12,   KC_F11,   KC_F10,   KC_F9,
 		
 		// row 4
 		// lthumb_i	lthumb_o	rthumb_o	rthumb_i
@@ -77,19 +80,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[3] = LAYOUT_split_3x5_2(
 		// row 1 right - left
 		// pinky	ring		middle		index		index+
-		KC_9,		KC_8,		KC_7,		KC_MINS,	KC_PSLS,
+    KC_SLSH,  KC_BSLS,  KC_EQL,   KC_MINS,  KC_NO,
 		// index+	index		middle		ring		pinky
 		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,
 
 		// row 2 right - left
 		// pinky	ring		middle		index		index+
-		KC_3,		KC_2,		KC_1,		KC_0,		KC_ESC,
+		KC_QUOT,  KC_SCLN,  KC_RBRC,  KC_LBRC,  KC_NO,
 		// index+	index		middle		ring		pinky
-		KC_ESC,		KC_LCTL,	KC_LSFT,	KC_LALT,	KC_LGUI,
+		KC_NO,		KC_LCTL,  KC_LSFT,  KC_LALT,  KC_LGUI,
 
 		// row 3 right - left
 		// pinky	ring		middle		index		index+
-		KC_6,		KC_5,		KC_4,		KC_EQL,		KC_NO,
+		KC_P,     KC_T,     KC_G,     KC_B,     KC_NO,
 		// index+	index		middle		ring		pinky
 		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,
 		
@@ -101,19 +104,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[4] = LAYOUT_split_3x5_2(
 		// row 1 right - left
 		// pinky	ring		middle		index		index+
-		KC_INS,		KC_DEL,		KC_HOME,	KC_END,		KC_NO,
+    KC_SCRL,  KC_INS,   KC_PGDN,  KC_PGUP,  KC_NO,
 		// index+	index		middle		ring		pinky
 		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,
 
 		// row 2 right - left
 		// pinky	ring		middle		index		index+
-		KC_RGHT,	KC_LEFT,	KC_UP,		KC_DOWN,	KC_ESC,
+    KC_RGHT,  KC_UP,    KC_DOWN,  KC_LEFT,  KC_NO,
 		// index+	index		middle		ring		pinky
-		KC_ESC,		KC_LCTL,	KC_LSFT,	KC_LALT,	KC_LGUI,
+		KC_NO,		KC_LCTL,	KC_LSFT,	KC_LALT,	KC_LGUI,
 
 		// row 3 right - left
 		// pinky	ring		middle		index		index+
-		KC_NO,		KC_NO,		KC_PGUP,	KC_PGDN,	KC_NO,
+		KC_ENT,		KC_CAPS,  KC_END,	  KC_HOME,	KC_NO,
 		// index+	index		middle		ring		pinky
 		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,
 		
@@ -129,8 +132,3 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 
 };
 #endif // defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
-
-
-
-
-
